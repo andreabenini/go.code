90759e0ce9e326fa582f6307c35d47a05957fb37
Ben <andreabenini@users.noreply.github.com>
2019-06-24 00:12:11 +0200

    Update map.go

--- src/map.go
+++ src/map.go
@@ -4,3 +4,9 @@ if element, exist := myMap[key]; exist {
 } else {
     fmt.Println("Element does not exists")
 }
+
+
+// interface{} -> map
+// content  : interface{}
+// mapValue : map[string]interface{}
+mapValue := content.(map[string]interface{})
